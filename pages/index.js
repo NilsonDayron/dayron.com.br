ff9446e4723ca707ed47e2574e3b40c732613e94
NilsonDayron <141246177+NilsonDayron@users.noreply.github.com>
2024-12-16 18:00:38 +0000

    atualiza a frase

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home () {
-    return <h1>Vitória, caso você esteja lenda essa mensagem saiba que seu namorado te ama independete de qualquer coisa e ele sente que não lhe merece. Espero que ele possa estar a sua altura conforme os dias passam. Falaram-me que era para escrever uma coisa para alguém importante =)</h1>
+    return <h1>Calma, respira. Um passo de cada vez. No seu tempo.</h1>
 }
 
 export default Home;
