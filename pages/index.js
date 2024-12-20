a137e34669da33e4bfccdd88a90c628c7f3d6e40
NilsonDayron <141246177+NilsonDayron@users.noreply.github.com>
2024-12-20 18:30:12 +0000

    atualizanndo novamente a frase

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home () {
-    return <h1>Calma, respira. Um passo de cada vez. No seu tempo.</h1>
+    return <h1>Calma, respira. Um passo de cada vez. No seu tempo. Um dia de cada na programação!</h1>
 }
 
 export default Home;
