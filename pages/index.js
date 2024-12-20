55e1cda0d22336e96fad9344026f2a2faaa8c485
NilsonDayron <141246177+NilsonDayron@users.noreply.github.com>
2024-12-20 18:24:02 +0000

    alterando o nome da frase

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home () {
-    return <h1>Calma, respira. Um passo de cada vez. No seu tempo.</h1>
+    return <h1>Calma, respira. Um passo de cada vez. No seu tempo. Continue nesse curso, você terá sucesso!</h1>
 }
 
 export default Home;
